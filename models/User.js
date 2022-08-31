60244bc5cea2d0c85167b7f081a8e0128ccad11d
Samuel Ercanbrack <sam.ercanbrack@outlook.com>
2022-08-30 19:57:24 -0600

    Finished userSchema and index.js in models

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require('mongoose')
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            properties: {}
+        },
+        thoughts: [thoughtSchema],
+        friends: [userSchema],
+    }
+    {
+        toJSON: {
+            getters: true,
+        }
+    }
+)
+
+const User = model('user', userSchema)
+
+module.exports = User
