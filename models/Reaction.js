1d634449225c68f77412fa90d00025eb4f553fe6
Samuel Ercanbrack <sam.ercanbrack@outlook.com>
2022-09-01 16:27:10 -0600

    finished reaction schema

---
+++ models/Reaction.js
@@ -0,0 +1,27 @@
+const { Schema, Types } = require('mongoose')
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Shcmea.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (date) => {
+                if (date) return date.toISOString().split("T")[0]
+            }
+        }
+    }
+)
